414a7eb38be5cc80102121d98335d94667cc30d1
jacobsteffenBYU <jacob.steffen@byu.edu>
2025-09-29 11:41:43 -0600

    updated to new standard

--- solutions/extra_02_args_kwargs.py
+++ solutions/extra_02_args_kwargs.py
@@ -1,20 +1,9 @@
-import os
-import platform
-
-def clear_screen():
-    """
-    Clears the terminal screen to make it easier to follow along with code.
-    """
-    if platform.system() == 'Windows':
-        os.system('cls')
-    else:
-        os.system('clear')
-
+from helper_functions import clear_screen
 clear_screen()
 
-# =======================
+# ==================
 # *ARGS and **KWARGS
-# =======================
+# ==================
 
 '''
 OVERVIEW
@@ -36,7 +25,10 @@ to as **kwargs
 # Write a function called sum_numbers takes any number of numbers and returns
 # their sum. Use *args to handle an unknown number of arguments.
 # (you don't have to call it args, but you do need the *)
+def sum_numbers(*args):
+    return sum(args)
 
+print(sum_numbers(10, 2, 3, 17)) 
 
 # 2. USING **KWARGS
 # Write a function called send_email. It always requires a "to" parameter.
@@ -44,3 +36,18 @@ to as **kwargs
 # importantance, etc.). Use **kwargs to allow the caller of the function to
 # insert whatever info they want to the email. Use kwargs.get to check if specific
 # info is there and return all the email info in a combined string.
+
+def send_email(to, **kwargs):
+    subject = kwargs.get('subject', 'No Subject')
+    body = kwargs.get('body', 'No Body Content')
+    sender = kwargs.get('sender', 'noreply@example.com')
+    important = kwargs.get('important', False)
+
+    email_info = f"To: {to}\nFrom: {sender}\nSubject: {subject}\nBody: {body}\nImportant: {important}"
+    return email_info
+
+# Sending an email with only the required parameter
+send_email('recipient@example.com')
+
+# Sending an email with additional optional parameters
+send_email('recipient@example.com', subject="Meeting Reminder", body="Don't forget our meeting tomorrow!", important=True)
